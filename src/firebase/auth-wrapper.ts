37adaea645721eb7b8eb6638fdaba568e476901b
gabcoroba <gabcoroba@gmail.com>
2023-09-15 18:04:53 -0400

    Adds firebase and firebase auth wrappers

---
+++ src/firebase/auth-wrapper.ts
@@ -0,0 +1,51 @@
+import firebase from "firebase/app";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export const _signUpWithEmailAndPassword = async (
+	email: any,
+	password: any,
+) => {
+	try {
+		const auth = getAuth();
+		const userCredential = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password,
+		);
+		return userCredential.user;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const _signInWithEmailAndPassword = async (
+	email: any,
+	password: any,
+) => {
+	try {
+		const auth = getAuth();
+		const userCredential = await signInWithEmailAndPassword(
+			auth,
+			email,
+			password,
+		);
+		return userCredential.user;
+	} catch (error) {
+		throw error;
+	}
+};
+
+export const signOut = async () => {
+	try {
+		const auth = getAuth();
+		await auth.signOut;
+	} catch (error) {
+		throw error;
+	}
+};
+
+// Add more authentication-related functions as needed
