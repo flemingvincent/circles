23f70c4b480f65fad265a11b32969ec4aa4a233b
gabcoroba <gabcoroba@gmail.com>
2023-09-19 17:16:10 -0400

    Switching android app config to web

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,19 +2,22 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 // @ts-ignore
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+//import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDZkGbZNNd6MG-eXBYhzcvBYL6YGsjn8lk",
 	authDomain: "circles-da7a6.firebaseapp.com",
 	projectId: "circles-da7a6",
 	storageBucket: "circles-da7a6.appspot.com",
-	//    messagingSenderId: "YOUR_MESSAGING_SENDER_ID", //This is for push notifications later
-	appId: "1:148136229772:android:4587fbaedf114db471b4a9",
+	messagingSenderId: "148136229772",
+	appId: "1:148136229772:web:9fdd46df41b282c971b4a9",
+	measurementId: "G-EYGKMYWXT5",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
 	persistence: getReactNativePersistence(AsyncStorage),
 });
+//const analytics = getAnalytics(app);
 
 export { auth };
