b23ba23e52b99dbfd0fbbd7920cff76e7ac252b8
pvanderlaat <pvanderlaat@gmail.com>
2023-09-19 22:10:40 -0400

    Revert "linting."
    This reverts commit 0ca3b1209d2a01bab807dcc92e988d19ed5b8529.

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,9 +1,9 @@
-import { forwardRef } from "react";
 import { TextInput, TextInputProps, View } from "react-native";
 import Animated, {
 	useAnimatedStyle,
 	withTiming,
 } from "react-native-reanimated";
+import { forwardRef } from 'react';
 
 import { Text } from "./Text";
 
@@ -16,62 +16,60 @@ interface InputProps extends TextInputProps {
 	error?: string;
 }
 
-export const Input = forwardRef(
-	(props: InputProps, ref: React.Ref<TextInput>) => {
-		const { icon, indicator, description, error, ...otherProps } = props;
-		const rErrorStyle = useAnimatedStyle(() => {
-			return {
-				opacity: error
-					? withTiming(1, {
-							duration: 350,
-					  })
-					: withTiming(0, {
-							duration: 350,
-					  }),
-			};
-		});
+export const Input = forwardRef((props: InputProps, ref: React.Ref<TextInput>) => {
+	const { icon, indicator, description, error, ...otherProps } = props;
+	const rErrorStyle = useAnimatedStyle(() => {
+		return {
+			opacity: error
+				? withTiming(1, {
+						duration: 350,
+				  })
+				: withTiming(0, {
+						duration: 350,
+				  }),
+		};
+	});
 
-		return (
-			<>
-				<View style={tw`flex-row items-center gap-x-4`}>
-					<TextInput
-						ref={ref}
-						style={[
-							tw`flex-1 body text-content-primary`,
-							{
-								fontFamily: "OpenRundeSemibold",
-							},
-						]}
-						autoCorrect={false}
-						autoCapitalize="none"
-						placeholderTextColor={tw.color("content-tertiary/50")}
-						{...props}
-					/>
-					{icon}
-				</View>
-				{indicator}
-				{description && (
-					<Text
-						variant="caption1"
-						weight="semibold"
-						style={[tw`text-content-tertiary mt-6`, !!indicator && tw`mt-4`]}
-					>
-						{description}
+	return (
+		<>
+			<View style={tw`flex-row items-center gap-x-4`}>
+				<TextInput
+					ref={ref}
+					style={[
+						tw`flex-1 body text-content-primary`,
+						{
+							fontFamily: "OpenRundeSemibold",
+						},
+					]}
+					autoCorrect={false}
+					autoCapitalize="none"
+					placeholderTextColor={tw.color("content-tertiary/50")}
+					{...props}
+				/>
+				{icon}
+			</View>
+			{indicator}
+			{description && (
+				<Text
+					variant="caption1"
+					weight="semibold"
+					style={[tw`text-content-tertiary mt-6`, !!indicator && tw`mt-4`]}
+				>
+					{description}
+				</Text>
+			)}
+			{error && (
+				<Animated.View
+					style={[
+						tw`bg-error/5 mt-4 px-4 py-2 rounded-full self-start`,
+						rErrorStyle,
+					]}
+				>
+					<Text variant="subheadline" style={tw`text-error`}>
+						{error}
 					</Text>
-				)}
-				{error && (
-					<Animated.View
-						style={[
-							tw`bg-error/5 mt-4 px-4 py-2 rounded-full self-start`,
-							rErrorStyle,
-						]}
-					>
-						<Text variant="subheadline" style={tw`text-error`}>
-							{error}
-						</Text>
-					</Animated.View>
-				)}
-			</>
-		);
-	},
-);
+				</Animated.View>
+			)}
+		</>
+	);
+})
