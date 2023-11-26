dddd7d4d72ea6eb7ecec9eebf55044cdc1df4bc5
gabcoroba <gabcoroba@gmail.com>
2023-11-26 13:43:18 -0500

    adding clipboard call to handler

--- src/components/Push.tsx
+++ src/components/Push.tsx
@@ -1,14 +1,22 @@
+import Clipboard from "expo-clipboard";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import { Platform } from "react-native";
 
 Notifications.setNotificationHandler({
-	handleNotification: async () => ({
-		shouldShowAlert: true,
-		shouldPlaySound: true,
-		shouldSetBadge: false,
-	}),
+	handleNotification: async (notification) => {
+		const invitationCode = notification.request.content.data?.invitationCode;
+
+		// Copy the invitation code to the clipboard
+		await Clipboard.setStringAsync(invitationCode || "");
+
+		return {
+			shouldShowAlert: true,
+			shouldPlaySound: true,
+			shouldSetBadge: false,
+		};
+	},
 });
 
 async function sendPushInviteCode(
